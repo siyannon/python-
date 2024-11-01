1ca702596f3abe30ff9e2e94d4f09e3f2a26ca9b
Xinlong Zhang <siyannon@gmail.com>
2024-11-01 17:25:52 +0800

    “目录文件遍历查找”

---
+++ FileTraversalAndSearch.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+	#指定要遍历的目录（起始点）
+	file_dir = './'
+	#使用os.walk()函数遍历指定目录，返回遍历到的路径、子目录、文件
+	for roots,dirs,files in os.walk(file_dir):
+		print("roots:",roots)
+		print("dirs:",dirs)
+		print("files",files)
+
+		#遍历当前目录下的每个文件，并且输出以“.exe”结尾的文件
+		for file in files:
+			if file.endswith('.exe'):
+				print(file)
